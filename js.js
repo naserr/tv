f0265183a3fadcae5c33fc0e6786e9aea4b3ab7f
Naserr <naser0rahmani@gmail.com>
2012-04-16 00:06:14 +0430

    j.s problem!

--- js.js
+++ js.js
@@ -1,15 +1,15 @@
 window.onload=function(){
 	
-	var bannerWidth = 980;
+	var bannerWidth = 790;
 	
-	var divScrl = document.getElementsByClassName('banner').item(0).getElementsByClassName('scroll').item(0);
+	var divBox = document.getElementsByClassName('banner').item(0).getElementsByClassName('box').item(0);
 	
-	var liBtns=document.getElementsByClassName('banner').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
+	var liBtns=document.getElementsByClassName('tv').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
 	for(var i=0;i<liBtns.length;i++){
 		(function(i){
 			liBtns.item(i).onclick=function(){
-				divScrl.style.left= (-i*bannerWidth) + 'px';
+				divBox.style.left= (-i*bannerWidth) + 'px';
 			}
 		})(i);
 	}
