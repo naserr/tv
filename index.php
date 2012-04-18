8250a8b65a7f6233d48b9efbd55ac0f0fd5cb839
Naserr <naser0rahmani@gmail.com>
2012-04-18 12:24:54 +0430

    12:24

--- index.php
+++ index.php
@@ -8,33 +8,46 @@
 </head>
 
 <body>
+	<div id="loading">
+			<div class="circle"></div>
+			<div class="circle1"></div>
+	</div>
+
 <div class="container">
 	<div class="banner">
     	<div class="box">
-			<div class="one"></div>
-			<div class="two"></div>
-        	<div class="three"></div>
-        	<div class="four"></div>
-        	<div class="five"></div>
-        	<div class="six"></div>
+				<?php
+				$counter = 0;
+					$dir = './img/banner';
+					$sup_type = array('jpg','png','jpeg','gif'); 
+
+					$d = dir($dir);
+					while( $f=$d->read() ){
+						$f_type = end (explode('.',$f) );
+						 if( !in_array(strtolower($f_type),$sup_type) ) continue;
+						echo "<div><img src='./img/banner/$f' /></div>";
+						$counter ++;
+					}
+				?>
 		</div>
 	</div>
 		<div class="tv">
 			<ul class="btn">
                 <li class="yek">1</li>
-                <li class="do"2></li>
+                <li class="do">2</li>
                 <li class="se">3</li>
                 <li class="char">4</li>
                 <li class="panj">5</li>
                 <li class="shish">6</li>
+                <li class="haft">7</li>
 			</ul>
 			<div class="selector"></div>
 			<div class="power"></div>
 			<div class="off"></div>
 			<!-- <div class="txt"> <img src="img/bgtxt.png" width="400" height="40"</div> -->
             <div class="pannel">
-                <div class="next"><p>Next</p></div>
-                <div class="back"><p>Back</p></div>
+                <div class="next"><p ></p></div>
+                <div class="back"><p></p></div>
                 <div class="dogme"><p></p></div>
                 
 			</div>
--- js.js
+++ js.js
@@ -1,21 +1,38 @@
+var bannerWidth = 598,
+currentSlide = 0,
+divBox, liBtns, picsLen, go2slide, nextSlide, prvSlide;
+
 window.onload=function(){
-		var bannerWidth = 598;
-	
+		document.getElementById('loading').style.display='none';
+
 	var divBox = document.getElementsByClassName('banner').item(0).getElementsByClassName('box').item(0);
 	
 	var liBtns=document.getElementsByClassName('tv').item(0).getElementsByClassName('btn').item(0).getElementsByTagName('li');
 	
-	var divRot=document.getElementsByClassName('selector').item(0);
+	go2slide = function (n){
+		if(n>=picsLen) n=0;
+		if(n<0) n=picsLen-1;
+		
+		divBox.style.left = -n*bannerWidth + 'px';
+		currentSlide=n;
+	};
+	nextSlide = function (){
+		go2slide(currentSlide+1)
+	};
+	prvSlide = function (){
+		go2slide(currentSlide-1)
+	};
 
-	for(var i=0;i<liBtns.length;i++){
-		(function(i){
-			liBtns.item(i).onclick=function(){
-				divBox.style.left= (-i*bannerWidth) + 'px';
-				divRot.style.webkitTransform = 'selector(20deg)';
+	for(var i=0;i<picsLen;i++){
+		(function(j){
+			liBtns.item(j).onclick=function(){
+				go2slide(j);
 			}
 		})(i);
 	}
-
+	
+//	setInterval(nextSlide,5000);
+	
 	var divPower= document.getElementsByClassName('power').item(0);
 	var divOff=document.getElementsByClassName('off').item(0);
 	divPower.onclick = function(){
